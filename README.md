5b0cdc3285d95773ccef3cc245270890c4105b05
Michal Nowotka <mnowotka@ebi.ac.uk>
2014-07-29 12:08:11 +0100

    initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-chembl_migrate
-==============
-
-Django custom management tool intended to perform data exports and migration of CheMBL database.
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'mnowotka'
+
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+if sys.version_info < (2, 7, 3) or sys.version_info >= (2, 7, 7):
+    raise Exception('ChEMBL software stack requires python 2.7.3 - 2.7.7')
+
+setup(
+    name='chembl_migrate',
+    version='0.5.7',
+    author='Michal Nowotka',
+    author_email='mnowotka@ebi.ac.uk',
+    description='Django custom management tool intended to perform data exports and migration of CheMBL database.',
+    url='https://www.ebi.ac.uk/chembl/',
+    license='CC BY-SA 3.0',
+    packages=['chembl_migrate',
+              'chembl_migrate.management',
+              'chembl_migrate.management.commands'],
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'Django==1.5.5',
+    ],
+    include_package_data=False,
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: Creative Commons :: Attribution-ShareAlike 3.0 Unported',
+                 'Operating System :: POSIX :: Linux',
+                 'Programming Language :: Python :: 2.7',
+                 'Topic :: Scientific/Engineering :: Chemistry'],
+    zip_safe=False,
+)
