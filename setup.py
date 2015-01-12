a49125fbd6ee71ee49a2749a598c758ee868469f
Michal Nowotka <mnowotka@ebi.ac.uk>
2015-01-12 10:08:13 +0000

    changing license, version bump

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 
 __author__ = 'mnowotka'
 
-import sys
-
 try:
     from setuptools import setup
 except ImportError:
@@ -12,17 +10,14 @@ except ImportError:
     use_setuptools()
     from setuptools import setup
 
-if sys.version_info < (2, 7, 3) or sys.version_info >= (3, 0, 0):
-    raise Exception('ChEMBL software stack requires python 2.7.3 - 3.0.0')
-
 setup(
     name='chembl_migrate',
-    version='0.5.8',
+    version='0.5.9',
     author='Michal Nowotka',
     author_email='mnowotka@ebi.ac.uk',
     description='Django custom management tool intended to perform data exports and migration of CheMBL database.',
     url='https://www.ebi.ac.uk/chembl/',
-    license='CC BY-SA 3.0',
+    license='Apache Software License',
     packages=['chembl_migrate',
               'chembl_migrate.management',
               'chembl_migrate.management.commands'],
@@ -35,7 +30,7 @@ setup(
                  'Environment :: Web Environment',
                  'Framework :: Django',
                  'Intended Audience :: Developers',
-                 'License :: Creative Commons :: Attribution-ShareAlike 3.0 Unported',
+                 'License :: OSI Approved :: Apache Software License',
                  'Operating System :: POSIX :: Linux',
                  'Programming Language :: Python :: 2.7',
                  'Topic :: Scientific/Engineering :: Chemistry'],
