e3308d899238d2dd0aa3fa297041a120c8d03d8b
Eloy Felix <eloyfelix@gmail.com>
2017-02-01 12:32:42 +0000

    cleaned fix and bump version

--- chembl_migrate/management/commands/migrate_table.py
+++ chembl_migrate/management/commands/migrate_table.py
@@ -108,9 +108,7 @@ class Command(BaseCommand):
                 else:
                     if not last_pk:
                         last_pk = model.objects.using(source_db).order_by(pk).only(pk).values_list(pk)[i][0]
-                    #original_data = model.objects.using(source_db).order_by(pk).filter(pk__gt=last_pk)[:size]
                     original_data = model.objects.using(source_db).filter(pk__gt=last_pk).order_by(pk)[:size]
-                #last_pk = original_data[size-1].pk
                 actual_size = len(original_data)
                 last_pk = original_data[actual_size-1].pk
 
